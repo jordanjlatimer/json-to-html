125169f6ba1bc1488ef454fb27052b340471b700
Jordan Latimer <jordanjlatimer@Jordans-Air.hsd1.ut.comcast.net>
2021-01-15 17:33:34 -0700

    updated index export method

---
+++ dist/slam.d.ts
@@ -0,0 +1,23 @@
+declare function slam(
+  arg1: string,
+  arg2?: string[],
+  arg3?: {
+    [key: string]: string;
+  }
+): string;
+declare function slam(
+  arg1: string,
+  arg2?: {
+    [key: string]: string;
+  },
+  arg3?: string[]
+): string;
+declare function slam(
+  arg1: string,
+  arg2?: {
+    [key: string]: string;
+  }
+): string;
+declare function slam(arg1: string, arg2?: string[]): string;
+declare function slam(arg1: string): string;
+export { slam };
